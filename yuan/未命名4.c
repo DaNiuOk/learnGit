6f7e537d09e641a0e3f9dbf7c8049d7d6a74de96
jiangyuan <375228099@qq.com>
2017-10-06 19:19:50 +0800

    添加gitignore

---
+++ yuan/未命名4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+ int main()
+{int s=68;
+ if(s<=100 && s>=80)
+{printf("你的成绩为优秀！");
+}else
+ if(s<=80 && s>=60)
+{printf("你的成绩为良好！");
+}else{printf("你的成绩不及格！");
+}
+ 
+ 
+ 
+ 
+ 
+  scanf("\n");	
+	return 0;
+ }
+	 
+	 
+
