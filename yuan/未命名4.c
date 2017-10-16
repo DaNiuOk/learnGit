d7450e919a1e7968ecc37ccc092fb73ef386c190
jiangyuan <375228099@qq.com>
2017-10-16 18:47:09 +0800

    梯形’ git add . git commit -m 梯形’

--- yuan/未命名4.c
+++ yuan/未命名4.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
 
  int main()
-{int s=68;
- if(s<=100 && s>=80)
-{printf("你的成绩为优秀！");
-}else
- if(s<=80 && s>=60)
-{printf("你的成绩为良好！");
-}else{printf("你的成绩不及格！");
+{int weekday=3;
+switch (weekday)
+{
+ case 1: 
+ printf("今天是周一！\n");
+ break;
+ case 2:
+ printf("今天是周二！\n");
+ break;
+ case 3:
+ printf("今天是周三！\n");
+ break;
+ case 4:
+ printf("今天是周四！\n");
+ break;
+ case 5:
+ printf("今天是周五！\n");
+ break; 
+ case 6:
+ printf("今天是周六！\n");
+ break;
+ case 7:
+ printf("今天是周日！\n");
+ break;
+ default:
+ printf("你写的数字是错误的！\n");
+ break;
 }
- 
- 
- 
- 
- 
-  scanf("\n");	
-	return 0;
- }
+	 
+	 
+}
+
+
+
 	 
 	 
 
