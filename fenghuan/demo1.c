8fc45ee0d5de9784352aa7de6f780561844a95d2
冯欢 <fenghuan@fenghuandeMac-mini.local>
2017-10-17 23:18:38 +0800

    循环倒排数字

---
+++ fenghuan/demo1.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+int main ()
+{
+    int line = 10;
+    int i, j;
+    for (i = 1; i < line; i++){
+        for (j = 1; j < line; j++){
+            if (i < line - j) {
+                printf(" ");
+            } else {
+                printf("%d", line - j);
+            }
+        }
+        printf("\n");
+    }
+    
+    //防止自动关闭
+    int stop;
+    printf("输入数字即可关闭。");
+    scanf("%d",&stop); 
+    return 0;
+} 
