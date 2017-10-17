87a0606b56086b72308b2f48306d5a6b18cf6c09
jiangjing <809998405@qq.com>
2017-10-17 16:37:10 +0800

    我也很无奈啊。。。

--- jing/task.c
+++ jing/task.c
@@ -1,10 +1,38 @@
 # include <stdio.h>
-int main()
+int main ()
 {
 	int a,b;
-	for (a=1;a<7;a++){
+	for (a=1;a<4;a++){
 		for (b=1;b<=a;b++){
-			printf("%d",b);
+			printf("*");
+		}
+		printf("\n");
+	}
+	int c,d;
+	for (c=3;c>0;c--) {
+		for (d=1;d<=c;d++){
+			printf("*");
+		}
+		printf("\n");
+	}
+	int e,f;
+	for (e=3;e>0;e--){
+		for (f=3;f>=e;f--){
+			printf("%d",e);
+		}
+		printf("\n");
+	} 
+	int g,h;
+	for (g=1;g<4;g++){
+		for (h=3;h>=g;h--){
+			printf("%d",h);
+		}
+		printf("\n");
+	}
+	int i,j;
+	for (i=1;i<10;i++){
+		for (j=1;j<=i;j++){
+			printf("%d*%d=%d ",j,i,i*j);
 		}
 		printf("\n");
 	}
