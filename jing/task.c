0153eb560d407585ba0b745c4e612ee7a28a21b3
jiangjing <809998405@qq.com>
2017-10-25 11:44:14 +0800

    。

--- jing/task.c
+++ jing/task.c
@@ -15,6 +15,7 @@ int main ()
 		}
 		printf("\n");
 	}
+	printf("\n");
 	int e,f;
 	for (e=3;e>0;e--){
 		for (f=3;f>=e;f--){
@@ -29,6 +30,7 @@ int main ()
 		}
 		printf("\n");
 	}
+	printf("\n");
 	int i,j;
 	for (i=1;i<10;i++){
 		for (j=1;j<=i;j++){
@@ -36,6 +38,7 @@ int main ()
 		}
 		printf("\n");
 	}
+	printf("\n");
 	 int k,l;
 int m=10;
 for (k=1;k<m;k++){
@@ -48,6 +51,33 @@ for (k=1;k<m;k++){
 	}
 	printf("\n");
 } 
+printf("\n");
+int n,o;
+for (n=1;n<10;n++){
+	for (o=1;o<10;o++){
+		if (n>3&&n<7&&o>3&&o<7){
+			printf(" ");
+		}else {
+			printf("*");
+		}
+	}
+	printf("\n");
+}
+printf("\n");
+int p,q;
+for (p=1;p<10;p++){
+	for (q=1;q<10;q++){
+		if (p>1&&p<9&&q>1&&q<9){
+			printf(" ");
+		}else {
+			printf("*");
+		}
+	}
+	printf("\n");
+}
+
+
+
 	
 	//·ÀÖ¹×Ô¶¯¹Ø±Õ
 	int stop;
