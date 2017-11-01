5f2df99d7a8f33f5059e528484fc02cec22076ce
jiangjing <809998405@qq.com>
2017-11-01 20:55:34 +0800

    菱形

--- jing/task.c
+++ jing/task.c
@@ -75,6 +75,18 @@ for (p=1;p<10;p++){
 	}
 	printf("\n");
 }
+int x,y;
+   for (x=1;x<12;x++){
+   	for (y=1;y<12;y++){
+   		if (y>5-x&&y>x-5&&y<x+5&&y<15-x){
+   			printf("*");
+		   } else {
+		   	printf(" ");
+		   }
+	   }
+	   printf("\n");
+   } 
+
 
 
 
